ff035c01df52de2eeed1f55fc55bc7bcf5de0c80
Andrew Bennett <hello@andrewbennett.us>
2019-04-05 12:50:31 -0400

    added login cmp

---
+++ Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+	<!-- Login Form -->
+	<!-- /Login Form -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+	data() {
+		return {
+            email: null,
+            password: null,
+            feedback: null
+		}
+	},
+	methods: {
+		login(){
+            // Check if the email and password are still null
+            if(this.email && this.password){
+                // ASYNC call that sends email and password
+                firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+                // Success
+                .then(user => {
+                    // Do something (Redirect with $router)
+                    console.log(user)
+                }).catch(err => {
+                    // Error
+                    this.feedback = err.message
+                })
+            } else {
+                // Email or password is empty
+                this.feedback = 'Please fill in both fields'
+            }
+        }
+	}
+}
+</script>
+
+<style></style>
