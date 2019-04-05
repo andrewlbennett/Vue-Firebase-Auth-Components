445179cb3c5321ebce86a9a06da8ea4caa1eae7b
Andrew Bennett <hello@andrewbennett.us>
2019-04-05 13:11:22 -0400

    added logout cmp

---
+++ Logout.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+	    <!-- Logout is usually a link or button sitting in another component -->
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase'
+
+export default {
+    name: 'Logout',
+	data() {
+		return {
+		}
+	},
+	methods: {
+		logout(){
+            firebase.auth.signOut.then(() => {
+                this.$router.push({ name: 'Signup' })
+            })
+        }
+	}
+}
+</script>
